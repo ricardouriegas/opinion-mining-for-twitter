63b9b08bc3c09b552bfacb1a082d1adce8ae9992
Ricardo Uriegas <risardo01@gmail.com>
2025-01-14 14:35:33 -0600

    cleaning the code

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.title("Twitter Analyzer")
 # description
 st.write("Given a topic, this app will analyze the sentiment of tweets about it and will return a graph about it.")
 
-# Entrada de texto
+# text input
 textField = st.text_input("Topic to analyze: ", "Python")
 
 # static data (just for simulation)
@@ -17,30 +17,19 @@ data = {
     "Neutral": 25
 }
 
-# Mostrar los datos en una gráfica
+# show the result in a pie chart
 if textField:
+    # header in page
     st.subheader(f"Result for the topic: {textField}")
     
-    # Crear la gráfica
+    # plot creation 
     labels = data.keys()
     values = data.values()
-    # fig, ax = plt.subplots()
-    # ax.bar(labels, values, color=['green', 'red', 'gray'])
-    # ax.set_title("Nivel de aceptación social")
-    # ax.set_ylabel("Cantidad de Tweets")
-    
-    # # Mostrar la gráfica
-    # st.pyplot(fig)
-
-    # grafica de pastel
     fig, ax = plt.subplots()
     ax.pie(values, labels=labels, autopct='%1.1f%%', startangle=90)
     ax.axis('equal')
-    ax.set_title(f"Social Acceptance Level for {textField}")
+    ax.set_title(f"Social Acceptance Level for {textField}") # title of the plot
+
+    # show the plot
     st.pyplot(fig)
-
-
-    # Mostrar datos
-    # st.write("**Distribución:**")
-    # st.json(datos)
 
